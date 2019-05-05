aa9866764b03991837225147e53e5db08f1674bb
Andrey <pyrinoff@gmail.com>
2019-05-05 16:37:22 +0300

    added mySortForKey

--- 2.php
+++ 2.php
@@ -4,19 +4,19 @@ function convertString(String $a, String $b)
 {
     $replace_number = 2;  //номер заменяемого вхождения
 
-/*
-    //Вариант 1 - через explode и пересборку строки
-    $exp = explode($b, $a, $replace_number + 1);
-    $count = count($exp);
-    if ($count <= $replace_number) return $a;    //если недостаточно вхождений - возвращаем исходный код
+    /*
+        //Вариант 1 - через explode и пересборку строки
+        $exp = explode($b, $a, $replace_number + 1);
+        $count = count($exp);
+        if ($count <= $replace_number) return $a;    //если недостаточно вхождений - возвращаем исходный код
 
-    $final = '';
-    for ($i = 0; $i < $count; $i++) {
-        $final .= $exp[$i]; //всегда добавляем элемент массива
-        if ($i == $replace_number - 1) $final .= strrev($b); //если это тот самый номер - реверсивную строку
-        else if ($i < $count - 1) $final .= $b; //либо обычную подстроку (если не последний элемент)
-    }
- */
+        $final = '';
+        for ($i = 0; $i < $count; $i++) {
+            $final .= $exp[$i]; //всегда добавляем элемент массива
+            if ($i == $replace_number - 1) $final .= strrev($b); //если это тот самый номер - реверсивную строку
+            else if ($i < $count - 1) $final .= $b; //либо обычную подстроку (если не последний элемент)
+        }
+     */
 
     //Вариант 2 - через индекс
     if (substr_count($a, $b) < $replace_number) return $a;
@@ -31,4 +31,20 @@ function convertString(String $a, String $b)
     return $final;
 }
 
+function mySortForKey($a, $b)
+{
+    if(!is_array($a)) throw new Exception('$a is not array');
+    foreach ($a as $k => $v) if (!isset($v[$b])) throw new Exception($k);
+
+    usort($a, function ($first, $second) use ($b) {
+        $result = $first[$b] - $second[$b];
+        if ($result < 0) return -1; //проверяем, чтобы избежать округления (для float)
+        else if ($result > 0) return 1;
+        else return 0;
+    });
+    return $a;
+}
+
 //var_dump(convertString('testANDtestANDtest', 'test'));
+//$someA = [['a' => 3, 'b' => 3], ['a' => 1, 'b' => 1], ['a' => 2, 'b' => 2]];
+//var_dump(mySortForKey($someA, 'b'));
